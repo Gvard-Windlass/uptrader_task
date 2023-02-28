1c49b30bda0fc5649af909becfd949a38688bca0
Gvard Windlass <gvard.windlass@gmail.com>
2023-02-28 11:27:26 +0300

    Add MenuItem model to admin

--- menu_maker/admin.py
+++ menu_maker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from menu_maker.models import MenuItem
 
-# Register your models here.
+
+@admin.register(MenuItem)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("name", "parent")
+    search_fields = ("name", "parent__name")
+    exclude = ("lft", "rgt")
