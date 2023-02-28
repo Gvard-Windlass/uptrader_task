900e0f84748763bf823b18b4ab9c619a5f895c3e
Gvard Windlass <gvard.windlass@gmail.com>
2023-02-28 12:20:11 +0300

    Add fixture for menu_maker tests

--- menu_maker/tests/test_models.py
+++ menu_maker/tests/test_models.py
@@ -3,6 +3,12 @@ from menu_maker.models import MenuItem
 
 
 class TestMenuItemModel(TestCase):
+    fixtures = ["menu_maker.json"]
+
     def test_create_menu_item(self):
         menu_item = MenuItem.objects.create(name="test item", lft=1, rgt=2)
         self.assertIsInstance(menu_item, MenuItem)
+
+    def test_get_menu_items(self):
+        items = MenuItem.objects.all()
+        self.assertEqual(len(items), 11)
