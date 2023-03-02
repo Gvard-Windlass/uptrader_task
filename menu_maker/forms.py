6edec248c9354b5230b58a8a168e80ef66e3d15f
Gvard Windlass <gvard.windlass@gmail.com>
2023-03-02 12:08:00 +0300

    Add help text to position field

--- menu_maker/forms.py
+++ menu_maker/forms.py
@@ -3,7 +3,11 @@ from menu_maker.models import MenuItem
 
 
 class MenuItemForm(forms.ModelForm):
-    position = forms.IntegerField(initial=-1, min_value=-1)
+    position = forms.IntegerField(
+        initial=-1,
+        min_value=-1,
+        help_text="0-based position amoung children, with -1 to insert last",
+    )
 
     class Meta:
         model = MenuItem
