f23c04852418748f2c3dbc26cf79be2bd5d0cf64
Gvard Windlass <gvard.windlass@gmail.com>
2023-03-02 12:14:27 +0300

    Add dynamic initial value to position field

--- menu_maker/forms.py
+++ menu_maker/forms.py
@@ -9,6 +9,12 @@ class MenuItemForm(forms.ModelForm):
         help_text="0-based position amoung children, with -1 to insert last",
     )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        position = self.instance.get_position()
+        if position:
+            self.fields["position"].initial = position[0] - 1
+
     class Meta:
         model = MenuItem
         exclude = ["lft", "rgt"]
